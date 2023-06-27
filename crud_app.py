6a3c16680a80a177d62e87261dc56ae23fc99dde
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2023-06-27 19:11:37 -0400

    Add functionality to create new items

--- crud_app.py
+++ crud_app.py
@@ -1,7 +1,15 @@
 import json
 
 def create_items():
-    pass
+    item = {}
+    item['name'] = input("Enter the item's name: ")
+    item['description'] = input("Enter the item's description")
+
+    with open('data.json', 'r+') as file:
+        data = json.load(file)
+        data.append(item)
+        file.seeek(0)
+        json.dump(data, file, indent=4)
 
 def read_items():
     pass
